79f18b80a627a3320921cc8f9ea8e0969fc43766
James Alexander <j.alexanderbryant@gmail.com>
2019-08-09 01:45:34 -0400

    Add testing with pytest.

--- server/app/app.py
+++ server/app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
+from app.blueprints.page import page
 
-def create_app():
+def create_app(settings_override=None):
     """
     Create a flask app using the app factory pattern
 
@@ -10,6 +11,11 @@ def create_app():
 
     app.config.from_object('config.settings')
     app.config.from_pyfile('settings.py', silent=True)
+
+    if settings_override:
+        app.config.update(settings_override)
+
+    app.register_blueprint(page)
 
     @app.route('/hello')
     def index():
--- server/config/settings.py
+++ server/config/settings.py
@@ -1,3 +1,3 @@
-DEBUG = True
-
+DEBUG = False
 HELLO = 'Hello, World! - (Find this string in the Main Config)'
+SERVER_NAME = 'localhost:8000'
